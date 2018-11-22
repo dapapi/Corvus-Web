0c6572367fe5025c393ba50ca479d2573ba18b98
Cxiaoyu <657466063@qq.com>
2018-11-22 21:23:49 +0800

    login stash

--- src/views/auth/login.vue
+++ src/views/auth/login.vue
@@ -1,78 +1,61 @@
 <template>
-    <div class="page" data-animsition-in="fade-in" data-animsition-out="fade-out">
-        <div class="page-content">
-            <div class="page-brand-info">
-                <div class="brand">
-                    <img class="brand-img" src="/images/logo@2x.png" alt="...">
-                    <h2 class="brand-text font-size-40">泰洋系</h2>
-                </div>
-                <p class="font-size-20">
-                    泰洋川禾(Mountain Top)基于对娱乐经济及跨屏融合的深度理解，公司汇集了娱乐领域、商务领域、金融领域资深人才，跨界搭建公司的核心运营团队。
-                </p>
-            </div>
+    <div class="page vertical-align text-center" data-animsition-in="fade-in" data-animsition-out="fade-out">
 
-            <div class="page-login-main" id="login-page" style="display: block">
-                <div class="brand hidden-md-up">
-                    <img class="brand-img" src="/images/logo-colored@2x.png" alt="...">
-                    <h3 class="brand-text font-size-40">泰洋系</h3>
-                </div>
-                <h3 class="font-size-24">登录</h3>
-                <p>请输入用户名、手机号或者邮箱进行登录</p>
-
-                <div class="form-group form-material floating" data-plugin="formMaterial">
-                    <input type="text" class="form-control empty" id="inputUsername" name="email">
-                    <label class="floating-label" for="inputUsername">用户名/手机号/邮箱</label>
-                </div>
-                <div class="form-group form-material floating" data-plugin="formMaterial">
-                    <input type="password" class="form-control empty" id="inputPassword" name="password">
-                    <label class="floating-label" for="inputPassword">密码</label>
-                </div>
-                <div class="form-group clearfix">
-                    <div class="checkbox-custom checkbox-inline checkbox-primary float-left">
-                        <input type="checkbox" id="inputCheckbox" name="checkbox">
-                        <label for="inputCheckbox">记住登录</label>
+        <div class="col-md-9 float-left">
+            aaa
+        </div>
+        <div class="col-md-3 float-left">
+            nnn
+        </div>
+        <div class="page-content vertical-align-middle">
+            <div class="panel">
+                <div class="panel-body">
+                    <ul class="nav nav-tabs nav-tabs-line" role="tablist">
+                        <li class="nav-item" role="presentation">
+                            <a class="nav-link active" data-toggle="tab" href="#forum-login"
+                               aria-controls="forum-base"
+                               aria-expanded="true" role="tab">账号登录</a>
+                        </li>
+                        <li class="nav-item" role="presentation">
+                            <a class="nav-link" data-toggle="tab" href="#forum-weixin-login"
+                               aria-controls="forum-present"
+                               aria-expanded="false" role="tab">微信登录</a>
+                        </li>
+                    </ul>
+                    <div class="tab-content nav-tabs-animate">
+                        <div class="tab-pane animation-fade active" id="forum-login" role="tabpanel">
+                            <div class="form-group example pt-10">
+                                <div class="input-group input-group-icon">
+                                    <span class="input-group-addon"
+                                          style="background-color: white;border-top: 1px solid #e0e0e0;border-left: 1px solid #E0E0E0;border-bottom: 1px solid #E0E0E0;border-right: 0">
+                                        <span class="icon md-smartphone-iphone" aria-hidden="true"></span>
+                                    </span>
+                                    <input type="text" class="form-control" placeholder="手机号">
+                                </div>
+                            </div>
+                            <div class="form-group example">
+                                <div class="input-group input-group-icon">
+                                    <span class="input-group-addon"
+                                          style="background-color: white;border-top: 1px solid #e0e0e0;border-left: 1px solid #E0E0E0;border-bottom: 1px solid #E0E0E0;border-right: 0">
+                                        <span class="icon md-lock" aria-hidden="true"></span>
+                                    </span>
+                                    <input type="text" class="form-control" placeholder="密码">
+                                </div>
+                            </div>
+                            <div class="form-group clearfix">
+                                <div class="checkbox-custom checkbox-inline checkbox-primary checkbox-lg float-left">
+                                    <input type="checkbox" id="inputCheckbox" name="remember">
+                                    <label for="inputCheckbox">记住账号</label>
+                                </div>
+                                <a class="float-right">忘记密码</a>
+                            </div>
+                        </div>
+                        <div class="tab-pane animation-fade" id="forum-weixin-login" role="tabpanel">
+                            weixin
+                        </div>
                     </div>
-                    <a class="float-right" href="/forgot">忘记密码？</a>
                 </div>
-                <button class="btn btn-primary btn-block" @click="initLoginForm">登录</button>
-
             </div>
-
-
-            <!--<div class="page-login-main" id="bind-telephone-page" style="display: none;">-->
-
-            <!--<div class="brand hidden-md-up">-->
-            <!--<img class="brand-img" src="/images/logo-colored@2x.png" alt="...">-->
-            <!--<h3 class="brand-text font-size-40">泰洋系</h3>-->
-            <!--</div>-->
-            <!--<h3 class="font-size-24">绑定手机号</h3>-->
-            <!--<p>请输入手机号和验证码进行手机号绑定</p>-->
-
-            <!--<form method="post" autocomplete="off" id="bind-form">-->
-            <!--<div class="form-group form-material floating" data-plugin="formMaterial">-->
-            <!--<input type="text" class="form-control empty" id="bindTelephoneInput" name="bind-telephone">-->
-            <!--<label class="floating-label" for="bindTelephoneInput">请输入手机号</label>-->
-            <!--<a id="send-code-btn" class="" href="javascript:void(0)">发送验证码</a>-->
-            <!--</div>-->
-            <!--<div class="form-group form-material floating" data-plugin="formMaterial">-->
-            <!--<input type="text" class="form-control empty" id="bindTelephoneCodeInout" name="bind-code">-->
-            <!--<label class="floating-label" for="bindTelephoneCodeInout">请输入验证码</label>-->
-            <!--</div>-->
-            <!--<button id="bind-submit-btn" class="btn btn-primary btn-block">绑定</button>-->
-            <!--</form>-->
-
-
-            <!--<footer class="page-copyright">-->
-            <!--<p>WEBSITE BY Mtop Creation Studio</p>-->
-            <!--<p>© 2018. All RIGHT RESERVED.</p>-->
-            <!--<div class="social">-->
-            <!--<a class="btn btn-icon btn-round social-twitter mx-5" href="/login">-->
-            <!--<i class="icon bd-twitter" aria-hidden="true"></i>-->
-            <!--</a>-->
-            <!--</div>-->
-            <!--</footer>-->
-            <!--</div>-->
-
         </div>
     </div>
 </template>
@@ -169,155 +152,25 @@
 </script>
 
 <style>
-    .page-login-v2 {
+    body {
         height: 100%;
-        overflow-x: hidden;
     }
 
-    .page-login-v2:before {
-        /*background-image: url("");*/
+    .page {
+        margin-left: 0;
+        height: 100%;
+        padding: 0;
     }
 
-    .page-login-v2.page-dark.layout-full:after {
-        background-color: rgba(33, 33, 33, .6);
+    #app {
+        height: 100%;
     }
 
-    .page-login-v2 .page-brand-info {
-        margin: 220px 100px 0 90px;
-    }
+    /*.input-group-icon {*/
+    /*background-color: white;*/
+    /*border-top: 1px solid #e0e0e0;*/
+    /*border-left: 1px solid #e0e0e0;*/
+    /*border-bottom: 1px solid #e0e0e0;*/
+    /*}*/
 
-    .page-login-v2 .page-brand-info .brand-img {
-        vertical-align: middle;
-    }
-
-    .page-login-v2 .page-brand-info .brand-text {
-        display: inline-block;
-        margin: 11px 0 11px 20px;
-        vertical-align: middle;
-    }
-
-    .page-login-v2 .page-brand-info p {
-        max-width: 650px;
-        opacity: .6;
-    }
-
-    .page-login-v2 .page-login-main {
-        position: absolute;
-        top: 0;
-        right: 0;
-        height: auto;
-        min-height: 100%;
-        padding: 150px 60px 180px;
-        color: #757575;
-        background: #fff;
-    }
-
-    .page-login-v2 .page-login-main .brand {
-        margin-bottom: 60px;
-    }
-
-    .page-login-v2 .page-login-main .brand-img {
-        vertical-align: middle;
-    }
-
-    .page-login-v2 .page-login-main .brand-text {
-        display: inline-block;
-        margin: 11px 0 11px 20px;
-        color: #3f51b5;
-        vertical-align: middle;
-    }
-
-    .page-login-v2 form {
-        width: 350px;
-        margin: 45px 0 20px;
-    }
-
-    .page-login-v2 form > button {
-        margin-top: 38px;
-    }
-
-    .page-login-v2 form a {
-        margin-left: 20px;
-    }
-
-    .page-login-v2 footer {
-        position: absolute;
-        right: 0;
-        bottom: 0;
-        left: 0;
-        margin: 50px 60px;
-        text-align: center;
-    }
-
-    .page-login-v2 .social .icon, .page-login-v2 .social .icon:hover, .page-login-v2 .social .icon:active {
-        color: #fff;
-    }
-
-    @media (min-width: 992px) {
-        .page-login-v2 .page-content {
-            padding-right: 500px;
-        }
-    }
-
-    @media (max-width: 991px) {
-        .page-login-v2 .page-login-main {
-            padding-top: 60px;
-        }
-    }
-
-    @media (min-width: 768px) and (max-width: 991px) {
-        .page-login-v2 .page-login-main {
-            padding-top: 80px;
-        }
-
-        .page-login-v2 .page-brand-info {
-            margin: 160px 0 0 35px;
-        }
-
-        .page-login-v2 .page-brand-info > p {
-            color: transparent;
-            opacity: 0;
-        }
-    }
-
-    @media (max-width: 767px) {
-        .page-login-v2 .page-login-main {
-            width: 100%;
-            padding-top: 60px;
-        }
-
-        .page-login-v2 form {
-            width: auto;
-        }
-    }
-
-    @media (max-width: 479px) {
-        .page-login-v2 .page-brand-info {
-            margin: 220px 0 0;
-        }
-
-        .page-login-v2 .page-login-main {
-            padding: 50px 30px 180px;
-        }
-
-        .page-login-v2 form {
-            width: auto;
-        }
-
-        .page-login-v2 footer {
-            margin: 50px 30px;
-        }
-    }
-
-    #send-code-btn {
-        position: absolute;
-        right: 8px;
-        color: #3f51b5;
-        top: 8px;
-        text-decoration: none;
-    }
-
-    #send-code-btn.disable {
-        color: #e0e0e0;
-    }
 </style>
