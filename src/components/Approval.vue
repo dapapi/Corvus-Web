e70fccee4e9e24284466eb9091ba7be9ac4b44e1
hanpeng <412640480@qq.com>
2018-11-26 20:25:34 +0800

    update

--- src/components/Approval.vue
+++ src/components/Approval.vue
@@ -18,17 +18,18 @@
             </section>
             <section class="page-aside-section">
               <div class="site-menubar-body" style="width:260px;">
-                <ul  class="menu pl-30">
-                  <div class="action" data-plugin="actionBtn" data-toggle="modal" data-target="#addModel" >
-                      <button type="button" class="site-action-toggle btn-raised btn  btn-floating waves-effect waves-classic">
-                          <i class="front-icon md-plus animation-scale-up" aria-hidden="true"></i>
-                      </button>
-                  </div> 
+                <ul  class="menu pl-20">
+                 
                   <li class="site-menu-item has-sub" v-for="item in list" :key="item.id" @click="switchMenu(item.id)">                     
                         <a href="javascript:void(0)">
                           <span class="icon md-caret-right font-size-20 mr-10 leftImg" :class="isShow == item.id?'anmite':''"></span>
                           <span class="site-menu-title">{{item.value}}</span>
                         </a>
+                         <div class="action" data-plugin="actionBtn" data-toggle="modal" data-target="#addModel" >
+                            <button type="button" class="site-action-toggle btn-raised btn  btn-floating waves-effect waves-classic">
+                                <i class="front-icon md-plus "  aria-hidden="true"></i>
+                            </button>
+                        </div> 
                         <ul class="site-menu-sub" v-show="isShow == item.id">
                           <li class="site-menu-item" v-for="item2 in  reportList" :key="item2.id">
                             <!-- <a href=""> -->
@@ -53,13 +54,23 @@
          role="dialog" tabindex="-1" id="addModel">
           <div class="modal-dialog modal-simple">
             <div class="modal-content">
-              <div class="modal-header">
+              <div class="modal-header" style="border:1px solid #ccc">
                 <button type="button" class="close" aria-hidden="true" data-dismiss="modal">
                   <i class="md-close" aria-hidden="true"></i>
                 </button>
                 <h4 class="modal-title">创建新审批报表</h4>
               </div>
-              <div class="modal-body">
+              <div class="modal-body" >
+                <div class="example">
+                <div class="pearls pearls-xs row" >
+                      <div class="pearl col-4" v-for="v in setp.data" :key="v.id" :class="setp.iSactive==v.id?'done':''">
+                        <span class="pearl-number">{{v.num}}</span>
+                        <span class="pearl-title">{{v.title}}</span>
+                      </div>
+                </div>
+                </div>
+                <template v-if="setp.iSactive=='1'">
+              
                 <div class="example">
                     <div class="col-md-2 text-right float-left pl-0">审批类型</div>
                     <div class="col-md-10 float-left pl-0">
@@ -67,24 +78,47 @@
                                     ></Selectors>
                     </div>
                 </div>
-              </div>
-              <div class="example">
-                  <div class="col-md-2 text-right float-left">报表名称</div>
-                  <div class="col-md-10 float-left pl-0">
-                      <input type="text" class="form-control" placeholder="请输入任务名称" >
-                  </div>
-              </div>
-              <div class="example">
-                <div class="col-md-2 text-right float-left">报表描述</div>
+                <div class="example">
+                    <div class="col-md-2 text-right float-left">报表名称</div>
                     <div class="col-md-10 float-left pl-0">
-                        <textarea class="form-control" name="taskDescription" id="" cols="30"
-                                  rows="5" title=""></textarea>
+                        <input type="text" class="form-control" placeholder="请输入任务名称" >
                     </div>
                 </div>
+                <div class="example">
+                  <div class="col-md-2 text-right float-left">报表描述</div>
+                      <div class="col-md-10 float-left pl-0">
+                          <textarea class="form-control" name="taskDescription" id="" cols="30"
+                                    rows="5" title="" placeholder="请填写审批报表描述"></textarea>
+                      </div>
+                </div>
+                </template> 
+                <template v-else-if="setp.iSactive=='2'"> 
+                <div class="example">
+                    <ListSelection></ListSelection>
+                </div> 
+                </template>
+                <template v-if="setp.iSactive=='3'">
+              
+                <div class="example">
+                    <div class="col-md-2 text-right float-left pl-0">审批类型</div>
+                    <div class="col-md-10 float-left pl-0">
+                        <Selectors  :placeholder="'请选择任务优先级'"
+                                    ></Selectors>
+                    </div>
+                </div>
+                
+                </template> 
+                </div>
               <div class="modal-footer">
-                <button class="btn btn-sm btn-white btn-pure" data-dismiss="modal">取消</button>
+                <button class="btn btn-sm btn-white btn-pure" data-dismiss="modal" v-show="iScancel" @click="cancel(setp.iSactive)">取消</button>
+                <button class="btn " type="submit" 
+                        data-toggle="modal" data-target="#addProject"  v-show="iSminus" @click="minus(--setp.iSactive)">上一步
+                </button>
                 <button class="btn btn-primary" type="submit" 
-                        data-toggle="modal" data-target="#addProject">确定
+                        data-toggle="modal" data-target="#addProject"  @click="next(++setp.iSactive)" v-show="iSplus">下一步
+                </button>
+                <button class="btn btn-primary" type="submit" 
+                        data-toggle="modal" data-target="#addProject"  v-show="iSkeep"> 保存
                 </button>
               </div>
               </div>
@@ -118,35 +152,41 @@ li a:hover {
   -webkit-transform: rotate(90deg); /* Safari 和 Chrome */
   -o-transform: rotate(90deg);
 }
-.animsition-link b{
-   position: absolute;
-   right:10px;
-   top:10px;
+.animsition-link b {
+  position: absolute;
+  right: 10px;
+  top: 10px;
 }
-.action{
+.action {
   display: none;
   position: absolute;
-  left:170px;
-  top:0px;
-  
+  left: 170px;
+  top: 0px;
 }
-.action button{
-   width: 20px;
-  height:20px;
-  line-height: 10px;
-  border: 1px solid#000;
+.action button {
+  width: 15px;
+  height: 15px;
+  border: 1px solid#484848;
+  position: relative;
 }
-.action button i{
-  font-size: 20px;
+.action button i {
+  font-size: 14px;
+  position: absolute;
+  top:-4px;
+  left:3px;
 }
-.menu:hover .action{
-    display: block;
+.menu li:first-child:hover .action {
+  display: block;
+}
+.bootstrap-select {
+    
+    width: 100% !important;
 }
 </style>
 <script>
 import fetch from "@/assets/utils/fetch";
 import config from "@/assets/js/config";
-
+import ListSelection from '../components/ListSelection'
 export default {
   data() {
     return {
@@ -165,20 +205,49 @@ export default {
           title: "报销",
           head: "审批添增"
         },
-        { title: "通用审批", 
-           head: "审批删除" },
+        {
+          title: "通用审批",
+          head: "审批删除"
+        },
         {
           title: "报销",
           head: "审批添增"
         },
         { title: "通用审批", head: "审批删除" }
       ],
-      isShow: "0"
-    };
+      isShow: "0",
+      setp:{
+         iSactive:"1",
+         data:[
+        {
+          id:"1",
+          title: "填写基本信息",
+          num:"1",
+          cont:[
+            {id: 1, key: "所属部门", field_type: 2},
+            {id: 2, key: "招聘职务", field_type: 1},
+            {id: 3, key: "招聘人数", field_type: 5},
+          ]
+        },
+        {
+          id:"2",
+          title: "填写基本信息",
+          num:"2",
+        },
+        {
+          id:"3",
+          title: "填写基本信息",
+          num:"3",
+        },
+      ],
+      },
+      iSminus:false,
+      iSplus:true,
+      iScancel:true,
+      iSkeep:false
+      }
   },
-  mounted() {
-    
-  },
+  mounted() {},
   methods: {
     switchMenu: function(id) {
       if (this.isShow == id) {
@@ -186,7 +255,40 @@ export default {
       } else {
         this.isShow = id;
       }
-    }
+    },
+    next(id){
+      if(id!=='1'){
+        this.iSminus=true
+      }
+      if(id=='3'){
+       this.iSplus=false;
+       this.iSkeep=true;
+       this.iScancel=true
+      }
+      if(id=='2'){
+        this.iScancel=false
+        
+      }
+    },
+    minus(id){
+      if(id=='1'){
+        this.iSminus=false;
+        this.iSplus=true;
+        this.iSkeep=false;
+        this.iScancel=true;
+      }
+      if(id=='2'){
+          this.iSplus=true;
+          this.iSkeep=false;
+          this.iScancel=false
+      }
+    },
+  },
+  cancel(){
+      
+  },
+  components: {
+    ListSelection
   }
 };
 </script>
---
+++ src/components/ListSelection.vue
@@ -0,0 +1,135 @@
+<template>
+    <div class="list pl-60">
+     <div class="left">
+        <div class="left-header pb-10">表单中所有字段</div>
+        <div class="left-cont">
+        <ul  >
+         <li v-for="(item,index) in list" :key="item.id"  @click="handleClick(index)">{{item.text}}</li>
+        </ul>
+        </div>
+     </div>
+     <div class="middle"></div>
+     <div class="right" >
+        <div class="right-header pb-10">显示字段</div>
+        <div class="right-cont">
+         <ul @click="back">
+            <li v-for="(item,index) in listright" :key="index" @click="back(index)">{{item.text}}</li>
+        </ul>
+        </div>
+     </div>
+    </div>
+</template>
+<script>
+export default {
+  name: "ListSelection",
+  data() {
+    return {
+      list: [
+        {
+          id: 1,
+          text: "申请事件"
+        },
+        {
+          id: 2,
+          text: "申请人"
+        },
+        {
+          id: 3,
+          text: "审批状态"
+        },
+        {
+          id: 4,
+          text: "审批人"
+        },
+        {
+          id: 5,
+          text: "申请事件"
+        },
+        {
+          id: 6,
+          text: "申请人"
+        },
+        {
+          id: 7,
+          text: "审批状态"
+        },
+        {
+          id: 8,
+          text: "审批人"
+        }
+      ],
+      listright:[]
+    };
+  },
+  methods:{
+      handleClick(i){
+         this.list.forEach((item,index)=>{
+             if(i==index){
+                this.listright.push(item);
+                this.list.splice(index,1)
+             }
+         })    
+      },
+      back(i){
+          this.listright.forEach((item,index)=>{
+             if(i==index){
+                this.list.push(item);
+                this.listright.splice(index,1)
+             }
+         }) 
+           
+      
+              
+      }
+
+  }
+};
+</script>
+<style scoped>
+li {
+  list-style: none;
+}
+.list{
+    display: flex;
+    position: relative;
+}
+.left-cont,.right-cont{
+    width: 200px;
+    height:300px;
+    border: 1px solid #ccc;
+    overflow: hidden;
+}
+.left ul li:hover,.right ul li:hover{
+    background: #eee;
+}
+.left ul,.right ul{
+    width: 100%;
+    height:300px;
+    overflow-y: scroll;
+    padding: 0;
+    text-indent: 2em;
+   
+}
+.left ul li,.right ul li{
+    height: 50px;
+    line-height: 50px;
+    width: 100%;
+}
+.middle{
+    position: absolute;
+    top:130px;
+    left:290px;
+    width: 0;
+    height: 0;
+    border-top: 5px solid transparent;
+    border-left: 10px solid #838383 ;
+    border-bottom: 5px solid transparent;
+}
+.right{
+    position: absolute;
+    right:30px;
+    top:0;
+}
+</style>
+
+
