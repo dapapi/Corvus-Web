2f339d9e54e62cafe5252438db472261aafa358d
bruce <1142634487@qq.com>
2018-12-07 21:00:37 +0800

    修改组织架构

--- src/views/organization/DepartmentItem.vue
+++ src/views/organization/DepartmentItem.vue
@@ -1,38 +1,16 @@
 <template>
     <div class="department-item">
-        <!-- <ul> -->
-            <!-- <li v-for="(item, index) in departmentList" :key="index">
-                <div class="name" :class="bgColor && index === showIndex ?'dark':''" @click="show(index)" :style="{paddingLeft: marginLeft + 'px'}">
-                    <i v-show="index === showIndex" class="icon md-caret-down"></i>
-                    <i v-show="index !== showIndex" class="icon md-caret-right"></i>
-                    <img src="../../assets/img/department@2x.png" />
-                    {{ item.name }} <span>({{count}})</span>
-                </div>
-                <ul v-show="index === showIndex">
-                    <li v-for="(_item, _index) in item.users.data" :key="_index" :style="{paddingLeft: marginLeft + 20 + 'px'}">
-                        {{ _item.name }}
-                    </li>
-                </ul>
-                <Department v-show="index === showIndex" :left="marginLeft" v-if="item.departments.data.length > 0" :departmentList="item.departments.data" :key="index" />
-            </li> -->
-            <!-- <li v-for="(item, index) in departmentList" :key="index">
-                <div class="name" :class="bgColor && index === showIndex ?'dark':''" @click="show(index)" :style="{paddingLeft: marginLeft + 'px'}">
-                    <i v-show="index === showIndex" class="icon md-caret-down"></i>
-                    <i v-show="index !== showIndex" class="icon md-caret-right"></i>
-                    <img src="../../assets/img/department@2x.png" />
-                    {{ data.name }} <span>({{count}})</span>
-                </div>
-            </li> -->
-        <!-- </ul> -->
         <div class="name" :style="{paddingLeft: paddingLeft + 'px'}" @click="showList">
             <i v-show="visible" class="icon md-caret-down"></i>
             <i v-show="!visible" class="icon md-caret-right"></i>
             <img src="../../assets/img/department@2x.png" />
-            {{ data.name }} <span>({{count}})</span>
+            {{ data.name }} <span>({{count}})</span> {{principal ? principal.name:'无负责人'}} 
+            <i class="icon md-plus add-member" style="float: right;line-height: 40px;" @click.stop="addMember"></i>
         </div>
         <ul v-show="visible">
             <li v-for="(_item, _index) in data.users.data" :key="_index" :style="{paddingLeft: paddingLeft + 40 + 'px'}">
                 {{ _item.name }}
+                <i class="icon md-eye" style="float: right;line-height: 40px;" @click.stop></i>
             </li>
         </ul>
          <template v-for="(item, index) in data.departments.data">
@@ -57,6 +35,10 @@ export default {
     computed: {
         paddingLeft () {
             return this.left || this.left === 0 ? this.left * 1 + 20 : 0
+        },
+        // 负责人
+        principal () {
+            return this.data.users.data.find(item => item.is_department_principal === 1)
         }
     },
     methods: {
@@ -73,6 +55,10 @@ export default {
                 })
             }
             return data.users.data.length + count
+        },
+        // 添加部门成员
+        addMember (id) {
+            alert('添加成员')
         }
     }
 }
@@ -87,10 +73,18 @@ ul {
         height: 40px;
         line-height: 40px;
         list-style: none;
+        i {
+            margin: 0 11px;
+            display: none;
+        }
         &:hover {
             background: #F5F5F5;
             color: #3f51b5;
             cursor: pointer;
+            i {
+                margin: 0 11px;
+                display: block;
+            }
         }
     }
 }
@@ -103,6 +97,9 @@ ul {
     &:hover {
         background: #F5F5F5;
         color: #3f51b5;
+        .add-member {
+            display: block;
+        }
     }
     i {
         margin: 0 14px;
@@ -113,6 +110,9 @@ ul {
         width: 14px;
         height: 12px;
         margin-right: 2px;
+    }
+    .add-member {
+        display: none;
     }
 }
 .dark {
