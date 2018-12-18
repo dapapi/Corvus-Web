b19c62ce158f92bc9aff11d4be8c46dfaca9928c
Xiao Wang <408136097@qq.com>
2018-12-18 20:57:10 +0800

    add alot of approval data-json

---
+++ src/views/approval/contractIndex/contractIndexData.js
@@ -0,0 +1,23 @@
+export const CONTRACT_INDEX_CONFIG = {
+    contractIndex :[
+        {
+            key:1,
+            value:'papi签约合同',
+        },{
+            key:2,
+            value:'papi解约合同',
+        },{
+            key:3,
+            value:'泰阳签约合同',
+        },{
+            key:4,
+            value:'泰阳解约合同',
+        },{
+            key:5,
+            value:'泰阳项目合同',
+        },{
+            key:6,
+            value:'papi项目合同',
+        }
+    ],
+}
