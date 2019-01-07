7167f9fc635bc4401d799703f3b008b2bf214cf2
sufang <sufang0722@163.com>
2019-01-07 15:57:12 +0800

    qsf

--- src/components/ConditionalInput.vue
+++ src/components/ConditionalInput.vue
@@ -48,6 +48,7 @@
                     setTimeout(function () {
                         _this.$refs.conditionSelector.setValue(_this.content)
                         _this.isInputShow = _this.content == _this.condition ? true : false
+                        _this.context = _this.inputContent;
                     }, 100)
                 }
             }
