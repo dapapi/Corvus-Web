166308b489a75e5da53c8c413deb6a866656e369
sufang <sufang0722@163.com>
2019-01-09 16:03:12 +0800

    qsf

--- src/components/DatePickerBrief.vue
+++ src/components/DatePickerBrief.vue
@@ -1,10 +1,5 @@
 <template>
     <div class="input-group date">
-        <input type="text" id="dateInput" class="form-control" title="" :placeholder="this.placeholder"
-            @change="getInputValue">
-        <span class="input-group-addon">
-            <i class="icon md-apps" aria-hidden="true"></i>
-        </span>
     </div> 
 </template>
 <script>
