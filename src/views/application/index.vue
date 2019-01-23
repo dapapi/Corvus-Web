67e9cfff866778bd9368e0201d14804baf46ba6f
hanpeng <412640480@qq.com>
2019-01-23 15:43:28 +0800

    bug

--- src/views/application/index.vue
+++ src/views/application/index.vue
@@ -74,20 +74,13 @@
             },
             getList:function(){
                 fetch('get',`${config.apiUrl}/getmodules`).then((res) => {
-<<<<<<< HEAD
                     console.log(res.data)
                     this.moduleList = res
-=======
-                    this.moduleList = res.data
->>>>>>> fc198f72ed7dc566a0b905522d4bc71663aa4ebf
                     let data={}
                     for (let i = 0; i < this.leftData.length; i++) {
                     if(this.leftData[i].id == 1){
                         for (let t = 0; t < res.data.length; t++) {
-<<<<<<< HEAD
                             console.log(res.data)
-=======
->>>>>>> fc198f72ed7dc566a0b905522d4bc71663aa4ebf
                             data={
                                 id:`${res.data[t].id}`,
                                 name:`${res.data[t].name}`,
