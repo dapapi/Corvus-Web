3917b54fdb640846a15c47623a8852a6aa828ea9
Xiao Wang <408136097@qq.com>
2019-01-24 11:52:18 +0800

    fix bug

--- src/views/approval/index.vue
+++ src/views/approval/index.vue
@@ -1,19 +1,25 @@
 <template>
     <div class="page page-aside-left">
         <div class="page-aside">
-            <Approval/>
+            <SecondLeft :leftData="leftData"/>
         </div>
         <router-view/>
     </div>
 </template>
 
 <script>
+import {APPROVAL_CONFIG} from '@/components/ForApproval/config.js'
     import Approval from '@/components/Approval.vue'
-
+    import SecondLeft from '@/components/SecondLeft.vue'
     export default {
         components: {
-            Approval
+            Approval,SecondLeft
         },
+        data(){
+            return{
+                leftData:APPROVAL_CONFIG.leftData
+            }
+        }
     }
 </script>
 
