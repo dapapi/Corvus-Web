b4d3ba027368e9f6e65fdac12dfca8aba4a0da7f
hanpeng <412640480@qq.com>
2019-03-06 17:14:45 +0800

    bug

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -11,30 +11,23 @@
                     <div data-role="content" class="scrollable-content" style="width: 259px;">
                         <section class="page-aside-section">
                             <h5 class="page-title pl-30 mb-20">仪表盘<i class="iconfont icon-xinjianmenhu pl-100"  data-toggle="modal" data-target="#dashboard"></i></h5>
-                                <div class="tree-view-menu">
-                                    <div class="tree-view-item">
-                                        <div class="level" :class="`level-${menu.level}`" v-for="menu in urlData" :key="menu.id">
-                                            <div class="list-group-item selected" v-if="menu.type ==='link'" >
-                                                <router-link class="link" v-bind:to="menu.url"  :class="menu.level>1?'pl-15':''">
-                                                {{menu.name}}
-                                                    <div class="drop-parent" style="position: absolute; right:2px;top:-8px;"
-                                                        >
-                                                        <i class="iconfont icon-gengduo1 font-size-20 parent" aria-hidden="true"
-                                                        data-toggle="dropdown" aria-expanded="false"
-                                                        style="cursor: pointer; float: right;line-height: 40px;">
-                                                        </i>
-                                                        <div class="dropdown-menu dropdown-menu-left" aria-labelledby="org-dropdown"
-                                                            role="menu" x-placement="bottom-start" style="min-width: 0;">
-                                                            <a class="dropdown-item" role="menuitem" data-toggle="modal"
-                                                            data-target="#addRole">新增角色</a>
-                                                            <a class="dropdown-item" role="menuitem" data-toggle="modal"
-                                                            data-target="#Rename">重命名</a>
-                                                            <a class="dropdown-item" role="menuitem" data-toggle="modal"
-                                                            data-target="#deleteGrouping">删除分组</a>
-                                                        </div>
-                                                    </div>
-                                                </router-link>
-                                            </div>
+                                <div class="level" :class="`level-${menu.level}`" v-for="menu in urlData" :key="menu.id">
+                                    <div class="list-group-item selected" v-if="menu.type ==='link'" >
+                                        <router-link class="link" v-bind:to="menu.url"  :class="menu.level>1?'pl-15':''">
+                                        {{menu.name}}
+                                        </router-link>     
+                                    </div>
+                                    <div class="drop-parent" style="position: absolute; right:30px;top:5px;">
+                                        <i class="iconfont icon-gengduo1 font-size-20 parent" aria-hidden="true"
+                                        data-toggle="dropdown" aria-expanded="false"
+                                        style="cursor: pointer; float: right;line-height: 40px;">
+                                        </i>
+                                        <div class="dropdown-menu dropdown-menu-left" aria-labelledby="org-dropdown"
+                                            role="menu" x-placement="bottom-start" style="min-width: 0;">
+                                            <a class="dropdown-item" role="menuitem" data-toggle="modal"
+                                            data-target="#Editor">编辑</a>
+                                            <a class="dropdown-item" role="menuitem" data-toggle="modal"
+                                            data-target="#Delete">删除</a>
                                         </div>
                                     </div>
                                 </div>
@@ -48,6 +41,7 @@
         </div>
         
         <router-view/>
+        <!-- 新建仪表盘 -->
          <div class="modal fade" id="dashboard" aria-hidden="true" aria-labelledby="addLabelForm" role="dialog"
              tabindex="-1" data-backdrop="static">
             <div class="modal-dialog modal-simple">
@@ -93,6 +87,77 @@
                 </div>
             </div>
         </div>
+        <!-- 编辑仪表盘 -->
+         <div class="modal fade" id="Editor" aria-hidden="true" aria-labelledby="addLabelForm" role="dialog"
+             tabindex="-1" data-backdrop="static">
+            <div class="modal-dialog modal-simple">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" aria-hidden="true" data-dismiss="modal">
+                            <i class="iconfont icon-guanbi" aria-hidden="true"></i>
+                        </button>
+                        <h4 class="modal-title">编辑</h4>
+                    </div>
+                    <div class="modal-body">
+                        <div class="example">
+                            <div class="col-md-2 text-right float-left">部门</div>
+                            <div class="col-md-10 float-left pl-0">
+                                 <Selectors @change="department"
+                                            :placeholder="'研发管理部'"></Selectors>
+                            </div>
+                        </div>
+                        <div class="example">
+                            <div class="col-md-2 text-right float-left">名称</div>
+                            <div class="col-md-10 float-left pl-0">
+                                 <input type="text" class="form-control" v-model="dashboardName" :placeholder="'研发管理部仪表盘'">
+                            </div>
+                        </div>
+                        <div class="example">
+                            <div class="col-md-2 text-right float-left">仪表盘描述</div>
+                            <div class="col-md-10 float-left pl-0">
+                                <textarea name="" rows="5" class="form-control" 
+                                          v-model="dashboard_describe" ></textarea>
+                            </div>
+                        </div>
+                         <div class="example">
+                            <div class="col-md-2 text-right float-left">成员</div>
+                            <div class="col-md-10 float-left pl-0">
+                                
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button class="btn btn-sm btn-white btn-pure" data-dismiss="modal">取消</button>
+                        <button class="btn btn-primary" type="submit" >确定</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--删除仪表盘-->
+        <div class="modal fade" id="Delete" aria-hidden="true" aria-labelledby="addLabelForm" role="dialog"
+             tabindex="-1" data-backdrop="static">
+            <div class="modal-dialog modal-simple">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" aria-hidden="true" data-dismiss="modal">
+                            <i class="iconfont icon-guanbi" aria-hidden="true"></i>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="example">
+                            <div class="col-md-12  pl-0">
+                                <p class="modal-title text-center">是否确定删除?</p>
+                                <p class="modal-title text-center">确定删除后将不会恢复</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button class="btn btn-sm btn-white btn-pure" data-dismiss="modal">取消</button>
+                        <button class="btn btn-danger" type="submit" >确定</button>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -110,10 +175,11 @@
                         isSelected: false,
                         level: 1,
                     },
+                    
                 ],
-                departmentDate:'',
-                dashboardName:'',
-                dashboard_describe:'',
+                departmentDate:'',//仪表盘部门值
+                dashboardName:'',//仪表盘名称
+                dashboard_describe:'',//仪表盘描述
                 
             }
         },
@@ -143,5 +209,8 @@
     }
     .selected {
        background-color:#eee
+    }
+    .level{
+        position: relative;
     }
 </style>
