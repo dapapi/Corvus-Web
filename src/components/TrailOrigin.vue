d76a6d26b8f3e892f468ebb78619fcf9a5e5852a
Cxiaoyu <657466063@qq.com>
2019-03-07 12:01:50 +0800

    1.2.20190306 1257 修复线索来源项目来源根据组织架构判断

--- src/components/TrailOrigin.vue
+++ src/components/TrailOrigin.vue
@@ -1,7 +1,8 @@
 <template>
     <div class="">
         <div class="text-right float-left pl-0"
-             :class="[detailPage? 'col-md-3': 'col-md-2',isEdit || alwaysShow ? 'line-fiexd-height': '','trail-origin-title']">{{typeName}}来源
+             :class="[detailPage? 'col-md-3': 'col-md-2',isEdit || alwaysShow ? 'line-fiexd-height': '','trail-origin-title']">
+            {{typeName}}来源
         </div>
         <div v-if="isEditSituation || alwaysShow">
             <!-- ⬆️判断是否永久显示 -->
@@ -18,7 +19,8 @@
                 </template>
                 <template v-else-if="(trailOrigin == 4 || trailOrigin == 5)">
                     <div class="col-md-6 float-left pr-0">
-                        <input-selectors @change="changeTrailOrigin" :placeholder='memberFinder' type="selector" class="test" :submit='submit'
+                        <input-selectors @change="changeTrailOrigin" :placeholder='memberFinder' type="selector"
+                                         class="test" :submit='submit'
                                          :propSelectMemberName='trailOriginPerson.name'></input-selectors>
                     </div>
                 </template>
@@ -42,15 +44,15 @@
 
 <script>
     import config from '@/assets/js/config'
-    import verify from '@/assets/utils/verify.js'
+    import verify from '@/assets/utils/verify'
     import {mapState} from 'vuex'
 
     export default {
-                //线索类型   {{什么}}线索  编辑状态   详情页样式  当前线索值    当前线索来源     永久显示           为了一个莫名其妙的需求加的
-        props: ['trailType', 'typeName', 'isEdit', 'detailPage', 'content', 'contentType', 'alwaysShow','submit'],
+        //线索类型   {{什么}}线索  编辑状态   详情页样式  当前线索值    当前线索来源     永久显示           为了一个莫名其妙的需求加的
+        props: ['trailType', 'typeName', 'isEdit', 'detailPage', 'content', 'contentType', 'alwaysShow', 'submit'],
         data() {
             return {
-                trailOriginArr: config.trailOrigin,     //线索类型列表
+                trailOriginArr: '',     //线索类型列表
                 trailOrigin: '',                         //线索类型
                 trailOriginPerson: '',                   //人员
                 email: '',                               //
@@ -63,6 +65,12 @@
         mounted() {
             if (this.contentType) {
                 this.trailOrigin = Number(this.contentType)
+            }
+            let organization_id = JSON.parse(Cookies.get('user')).organization_id
+            if (organization_id == 412) {
+                this.trailOriginArr = config.trailBloggerOrigin
+            } else {
+                this.trailOriginArr = config.trailOrigin
             }
         },
         computed: {
@@ -119,17 +127,6 @@
                 }
 
             },
-            //线索类型为4时切换博主
-            trailType: function () {
-                this.trailOriginArr = config.trailOrigin
-                if (this.trailType == 4) {
-                    this.trailOriginArr = config.trailBloggerOrigin
-                }
-                this.$nextTick(() => {
-                    $('.selectpicker').selectpicker('render');
-                    $('.selectpicker').selectpicker('refresh');
-                })
-            },
             //线索类型提交
             trailOrigin: function () {
                 this.$emit('changeTrailOrigin', this.trailOrigin)
@@ -169,6 +166,7 @@
     .line-fiexd-height {
         line-height: 34px;
     }
+
     /* .assistor{
         width: 100%;
     } */
