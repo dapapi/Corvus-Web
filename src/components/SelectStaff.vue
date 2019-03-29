007a7df25daaa7956ff6f0e3e4649cdca047e4b2
hanpeng <412640480@qq.com>
2019-03-29 20:20:05 +0800

    1.3.20190329艺人隐私

--- src/components/SelectStaff.vue
+++ src/components/SelectStaff.vue
@@ -133,6 +133,16 @@ export default {
                     return this.$store.state.incubationInfo
                 }else if(this.type === 'bill'){
                     return this.$store.state.billInfo
+                }else if(this.type === 'birthday'){
+                    return this.$store.state.birthdayInfo
+                }else if(this.type === 'star_risk_point'){
+                    return this.$store.state.star_risk_pointInfo
+                }else if(this.type === 'phone'){
+                    return this.$store.state.phoneInfo
+                }else if(this.type === 'wechat'){
+                    return this.$store.state.wechatInfo
+                }else if(this.type === 'email'){
+                    return this.$store.state.emailInfo
                 }else if(this.otherslot){
                     return this.$store.state.otherSlot
                 }
@@ -181,7 +191,17 @@ export default {
         participantInfo = this.$store.state.incubationInfo;
       } else if (this.type === 'bill') {
         participantInfo = this.$store.state.billInfo;
-      } else if (this.otherslot) {
+      }else if(this.type === 'birthday'){
+        participantInfo = this.$store.state.birthdayInfo
+    }else if(this.type === 'star_risk_point'){
+        participantInfo = this.$store.state.star_risk_pointInfo
+    }else if(this.type === 'phone'){
+        participantInfo = this.$store.state.phoneInfo
+    }else if(this.type === 'wechat'){
+        participantInfo = this.$store.state.wechatInfo
+    }else if(this.type === 'email'){
+        participantInfo = this.$store.state.emailInfo
+    }else if (this.otherslot) {
         participantInfo = this.$store.state.otherSlot;
       } else {
         participantInfo = this.$store.state.newParticipantsInfo;
@@ -220,6 +240,16 @@ export default {
           participantInfo = this.$store.state.incubationInfo;
         } else if (this.type === 'bill') {
           participantInfo = this.$store.state.billInfo;
+        }else if(this.type === 'birthday'){
+        participantInfo = this.$store.state.birthdayInfo
+        }else if(this.type === 'star_risk_point'){
+            participantInfo = this.$store.state.star_risk_pointInfo
+        }else if(this.type === 'phone'){
+            participantInfo = this.$store.state.phoneInfo
+        }else if(this.type === 'wechat'){
+            participantInfo = this.$store.state.wechatInfo
+        }else if(this.type === 'email'){
+            participantInfo = this.$store.state.emailInfo
         } else if (this.otherslot) {
           participantInfo = this.$store.state.otherSlot;
         } else {
