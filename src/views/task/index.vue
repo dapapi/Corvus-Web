ef51529f643a01e2a79598189f62bc9e92330540
bruce <1142634487@qq.com>
2019-03-31 13:54:14 +0800

    修改任务客户 分页展示问题

--- src/views/task/index.vue
+++ src/views/task/index.vue
@@ -278,7 +278,7 @@
                     } else {
                         this.current_page = response.current_page;
                         this.total = response.total;
-                        this.total_pages = response.per_page != 0 ? parseInt(response.total / response.per_page) : 0;
+                        this.total_pages = response.per_page != 0 ? Math.ceil(response.total / response.per_page) : 1;
                     }
                     this.isLoading = false;
                 });
